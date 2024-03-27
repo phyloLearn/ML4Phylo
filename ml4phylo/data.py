eac160fe0af045b928311121530b19d0fedcd0e3
MiguelRaposo49456 <A49456@alunos.isel.pt>
2024-03-27 15:56:40 +0000

    Added the encoding script and data functions

---
+++ ml4phylo/data.py
@@ -0,0 +1,65 @@
+from typing import Dict, List, Tuple
+
+import numpy as np
+import torch
+from Bio import SeqIO
+
+AMINO_ACIDS = np.array(list("ARNDCQEGHILKMFPSTWYVX-"))
+
+def load_alignment(path: str) -> Tuple[torch.Tensor, List[str]]:
+    """Loads an alignment into a tensor digestible by the Phyloformer network
+
+    Parameters
+    ----------
+    path : str
+        Path to a fasta file containing the alignment
+
+    Returns
+    -------
+    Tuple[torch.Tensor, List[str]]
+        a tuple containing:
+         - a tensor representing the alignment (shape 22 * seq_len * n_seq)
+         - a list of ids of the sequences in the alignment
+
+    """
+
+    tensor = []
+    parsed = _parse_alignment(path)
+    for sequence in parsed.values():
+        tensor.append(
+            torch.from_numpy(_sequence_to_one_hot(sequence)).t().view(22, 1, -1)
+        )
+
+    return torch.cat(tensor, dim=1).transpose(-1, -2), list(parsed.keys())
+
+
+def _parse_alignment(path: str) -> Dict[str, str]:
+    """Parser a fasta alignment
+
+    Parameters
+    ----------
+    path : str
+        Path to .fasta alignment file
+
+    Returns
+    -------
+    Dict[str,str]
+        A dictionnary with ids as keys and sequence as values
+    """
+    return {record.id: str(record.seq) for record in SeqIO.parse(path, format="fasta")}
+
+
+def _sequence_to_one_hot(seq: str) -> np.ndarray:
+    """Encode an amino acid sequence with one-hot encoding
+
+    Parameters
+    ----------
+    seq : str
+        Sequence of amino acids to encode
+
+    Returns
+    -------
+    np.ndarray
+        Encoded sequence (shape 22\*seq_len)
+    """
+    return np.array([(AMINO_ACIDS == aa).astype(int) for aa in seq])
---
+++ ml4phylo/scripts/alignment_encoding.py
@@ -0,0 +1,42 @@
+import argparse
+import os
+
+from tqdm import tqdm
+
+from ml4phylo.data import load_alignment
+
+
+def is_fasta(path: str) -> bool:
+    return path.lower().endswith("fa") or path.lower().endswith("fasta")
+
+
+def alignment_encoding(aln_dir: str):
+    for aln in (pbar := tqdm([file for file in os.listdir(aln_dir) if is_fasta(file)])):
+        base = aln.split(".")[0]
+        pbar.set_description(f"Processing {base}")
+
+        load_alignment(os.path.join(aln_dir, aln))
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description=(
+            "Predict phylogenetic trees from MSAs "
+            "using the Phyloformer neural network"
+        )
+    )
+    parser.add_argument(
+        "alidir",
+        type=str,
+        help="path to input directory containing the\
+    .fasta alignments",
+    )
+    args = parser.parse_args()
+
+    alignment_encoding(args.alidir)
+
+    print("\nDone!")
+
+
+if __name__ == "__main__":
+    main()
