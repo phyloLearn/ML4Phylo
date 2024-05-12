6a6fa4d9202f06b0d5a81b78bcbc81e2b6bbdd3a
MiguelRaposo49456 <A49456@alunos.isel.pt>
2024-05-12 19:18:18 +0100

    Started working on typing data encoder
    Co-authored-by: GoncaloSilva49451 <GoncaloSilva49451@users.noreply.github.com>

--- ml4phylo/utils.py
+++ ml4phylo/utils.py
@@ -1,16 +1,25 @@
+from re import sub, compile
+
 def println(txt, param):
     print(txt + "\n", param, "\n")
 
 def split_typing_data(input_file, output_prefix, number_of_files, lines_per_file):
+    # Compile the regular expression
+    pattern = compile(r'[a-zA-Z]+-\d+ [a-zA-Z]+')
+
     with open(input_file, 'r') as f:
-        lines = f.readlines()
+        header = sub("\w*complex", "", next(f))
 
-    header = lines[0]  # Save the header line
-    lines = lines[1:]  # Remove the header from the lines
+        for i in range(number_of_files):
+            output_file = f"{output_prefix}_{i + 1}.txt"
+            
+            with open(output_file, 'w') as out:
+                out.write(header)  # Write the header to each output file
 
-    for i in range(0, number_of_files*lines_per_file, lines_per_file):
-        output_file = f"{output_prefix}_{i//lines_per_file + 1}.txt"
-        with open(output_file, 'w') as f:
-            f.write(header)  # Write the header to each output file
-            f.writelines(lines[i:i+lines_per_file])
-            f.close()
+                for _ in range(lines_per_file):
+                    line = next(f, None)
+                    if line is None:
+                        break  # End of input file
+
+                    line = pattern.sub("", line)
+                    out.write(line)
